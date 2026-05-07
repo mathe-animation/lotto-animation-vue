f47414e6980fdb8bf32cb3fec1cc5a466be74113
Ernest Bernest <ernestbernest@MacBook-Pro-von-User.local>
2026-05-07 16:20:29 +0200

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-card style="margin: auto" width="400">
+    <v-responsive
+      :style="{ background: `rgb(${red}, ${green}, ${blue})` }"
+      height="300px"
+    ></v-responsive>
+
+    <v-card-text>
+      <v-slider
+        v-model="red"
+        :max="255"
+        :step="1"
+        class="ma-4"
+        label="R"
+        hide-details
+      >
+        <template v-slot:append>
+          <v-text-field
+            v-model="red"
+            density="compact"
+            style="width: 80px"
+            type="number"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
+        </template>
+      </v-slider>
+
+      <v-slider
+        v-model="green"
+        :max="255"
+        :step="1"
+        class="ma-4"
+        label="G"
+        hide-details
+      >
+        <template v-slot:append>
+          <v-text-field
+            v-model="green"
+            density="compact"
+            style="width: 80px"
+            type="number"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
+        </template>
+      </v-slider>
+
+      <v-slider
+        v-model="blue"
+        :max="255"
+        :step="1"
+        class="ma-4"
+        label="B"
+        hide-details
+      >
+        <template v-slot:append>
+          <v-text-field
+            v-model="blue"
+            density="compact"
+            style="width: 80px"
+            type="number"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
+        </template>
+      </v-slider>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const red = ref(64)
+  const green = ref(128)
+  const blue = ref(10)
+</script>
+
+<style scoped>
+@reference "../styles/tailwind.css";
+
+/*
+  1. mixing helper classes and @apply for demonstration purposes only
+  2. the classes below are NOT wrapped in any CSS layer, so they "win" over everything else
+*/
+.hero-card {
+  @apply py-3 md:pr-[120px] w-full transition-none;
+}
+
+:deep(.v-card) {
+  @apply bg-gray-200;
+  @apply dark:bg-black dark:bg-linear-to-r dark:from-primary/50 dark:to-primary/30 dark:text-white/80;
+}
+</style>
