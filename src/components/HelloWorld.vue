eb955d5b8c94113866765b99aac7461ac387d262
Ernest Bernest <ernestbernest@MacBook-Pro-von-User.local>
2026-05-07 17:00:34 +0200

    added title

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,67 +1,32 @@
 <template>
+
   <v-card style="margin: auto" width="600">
-
+    <h1
+        class="relative top-0 w-fit py-4 justify-center flex bg-gradient-to-r items-center from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-6xl font-extrabold text-transparent text-center select-auto">
+        LOTTO-Animation
+    </h1>
     <v-card-text>
-      <v-slider
-        v-model="k_slider"
-        :min="1"
-        :max="30"
-        :step="1"
-        class="ma-4"
-        label="k: Anzahl de gezogenen Kugeln"
-        hide-details
-      >
+      <v-slider v-model="k_slider" :min="1" :max="30" :step="1" class="ma-4" label="k: Anzahl der gezogenen Kugeln"
+        hide-details>
         <template v-slot:append>
-          <v-text-field
-            v-model="k_slider"
-            density="compact"
-            style="width: 120px"
-            type="number"
-            variant="outlined"
-            hide-details
-          ></v-text-field>
+          <v-text-field v-model="k_slider" density="compact" style="width: 120px" type="number" variant="outlined"
+            hide-details></v-text-field>
         </template>
       </v-slider>
 
-      <v-slider
-        v-model="n_slider"
-        :min="1"
-        :max="100"
-        :step="1"
-        class="ma-4"
-        label="n: Anzahl der Kugeln insgesamt"
-        hide-details
-      >
+      <v-slider v-model="n_slider" :min="1" :max="100" :step="1" class="ma-4" label="n: Anzahl der Kugeln insgesamt"
+        hide-details>
         <template v-slot:append>
-          <v-text-field
-            v-model="n_slider"
-            density="compact"
-            style="width: 120px"
-            type="number"
-            variant="outlined"
-            hide-details
-          ></v-text-field>
+          <v-text-field v-model="n_slider" density="compact" style="width: 120px" type="number" variant="outlined"
+            hide-details></v-text-field>
         </template>
       </v-slider>
 
-      <v-slider
-        v-model="count_trials"
-        :min="1000"
-        :max="500000000"
-        :step="1000"
-        class="ma-4"
-        label="Anzahl der gekauften LOTTO-Scheine"
-        hide-details
-      >
+      <v-slider v-model="count_trials" :min="1000" :max="500000000" :step="1000" class="ma-4"
+        label="Anzahl der gekauften LOTTO-Scheine" hide-details>
         <template v-slot:append>
-          <v-text-field
-            v-model="count_trials"
-            density="compact"
-            style="width: 120px"
-            type="number"
-            variant="outlined"
-            hide-details
-          ></v-text-field>
+          <v-text-field v-model="count_trials" density="compact" style="width: 120px" type="number" variant="outlined"
+            hide-details></v-text-field>
         </template>
       </v-slider>
     </v-card-text>
@@ -69,11 +34,11 @@
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
+import { ref } from 'vue'
 
-  const k_slider = ref(6)
-  const n_slider = ref(49)
-  const count_trials = ref(10000)
+const k_slider = ref(6)
+const n_slider = ref(49)
+const count_trials = ref(10000)
 </script>
 
 <style scoped>
