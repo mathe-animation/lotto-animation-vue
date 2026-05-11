cf8c42d850eb248a696ff48cf908775fa6e2d1e3
Ernest Bernest <ernestbernest@MacBook-Pro-von-User.local>
2026-05-11 19:16:09 +0200

    Improving start stop buttons

--- src/components/SimulationConfiguration.vue
+++ src/components/SimulationConfiguration.vue
@@ -1,32 +1,71 @@
 <template>
-
   <v-card style="margin: auto" width="600">
     <h1
-      class="relative top-0 w-fit py-4 justify-center flex bg-gradient-to-r items-center from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-6xl font-extrabold text-transparent text-center select-auto">
+      class="relative top-0 w-fit py-4 justify-center flex bg-gradient-to-r items-center from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-6xl font-extrabold text-transparent text-center select-auto"
+    >
       LOTTO: 6 aus 49
     </h1>
     <v-card-text>
-      <v-slider v-model="k_slider" :min="1" :max="30" :step="1" class="ma-4"
-        label="k: Anzahl der gezogenen Kugeln" hide-details>
+      <v-slider
+        v-model="k_slider"
+        :min="1"
+        :max="30"
+        :step="1"
+        class="ma-4"
+        label="k: Anzahl der gezogenen Kugeln"
+        hide-details
+      >
         <template v-slot:append>
-          <v-text-field v-model="k_slider" density="compact" style="width: 120px" type="number" variant="outlined"
-            hide-details></v-text-field>
+          <v-text-field
+            v-model="k_slider"
+            density="compact"
+            style="width: 120px"
+            type="number"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
         </template>
       </v-slider>
 
-      <v-slider v-model="n_slider" :min="1" :max="100" :step="1" class="ma-4"
-        label="n: Anzahl der Kugeln insgesamt" hide-details>
+      <v-slider
+        v-model="n_slider"
+        :min="1"
+        :max="100"
+        :step="1"
+        class="ma-4"
+        label="n: Anzahl der Kugeln insgesamt"
+        hide-details
+      >
         <template v-slot:append>
-          <v-text-field v-model="n_slider" density="compact" style="width: 120px" type="number" variant="outlined"
-            hide-details></v-text-field>
+          <v-text-field
+            v-model="n_slider"
+            density="compact"
+            style="width: 120px"
+            type="number"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
         </template>
       </v-slider>
 
-      <v-slider v-model="count_trials" :min="1000" :max="500000000" :step="1000"
-        class="ma-4" label="Anzahl der gekauften LOTTO-Scheine" hide-details>
+      <v-slider
+        v-model="count_trials"
+        :min="1000"
+        :max="500000000"
+        :step="1000"
+        class="ma-4"
+        label="Anzahl der gekauften LOTTO-Scheine"
+        hide-details
+      >
         <template v-slot:append>
-          <v-text-field v-model="count_trials" density="compact" style="width: 120px" type="number" variant="outlined"
-            hide-details></v-text-field>
+          <v-text-field
+            v-model="count_trials"
+            density="compact"
+            style="width: 120px"
+            type="number"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
         </template>
       </v-slider>
     </v-card-text>
@@ -35,139 +74,141 @@
         <svg-icon type="mdi" :path="pathRestart"></svg-icon>
         Simulation stoppen und neustarten
     </v-btn>-->
-  <v-btn @click="start_stop_animation" rounded="0">
-    <svg-icon type="mdi" :path="pathStart" :class="{ hidden: playIsHidden }"></svg-icon>
-    <svg-icon type="mdi" :path="pathStop" class="" :class="{ hidden: stopIsHidden }"></svg-icon>
-    {{ start_stop_text }}
+  <v-btn @click="start_simulation" rounded="0" :class="{ hidden: playIsHidden }">
+    <svg-icon
+      type="mdi"
+      :path="pathStart"
+    ></svg-icon>
+    {{ start_text }}
+  </v-btn>
+    <v-btn @click="stop_simulation" rounded="0" :class="{ hidden: stopIsHidden }">
+    <svg-icon
+      type="mdi"
+      :path="pathStop"
+      class=""
+    ></svg-icon>
+    {{ stop_text }}
   </v-btn>
   <v-card class="card-simulation-status">{{ status_text }}</v-card>
-
   <v-card class="card-simulation-status">Anzahl der Versuche</v-card>
   <v-card class="card-simulation-status">{{ trial_counter_value }}</v-card>
 </template>
 
-<script lang="ts" >
-import SvgIcon from '@jamescoyle/vue-icon'
-import { mdiRestart } from '@mdi/js'
-import { mdiPlayCircleOutline } from '@mdi/js';
-import { mdiStopCircleOutline } from '@mdi/js';
-
+<script lang="ts">
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiRestart } from "@mdi/js";
+import { mdiPlayCircleOutline } from "@mdi/js";
+import { mdiStopCircleOutline } from "@mdi/js";
 
 export default {
   name: "my-cool-component",
 
   components: {
-    SvgIcon
+    SvgIcon,
   },
   data() {
     return {
       pathRestart: mdiRestart,
       pathStart: mdiPlayCircleOutline,
       pathStop: mdiStopCircleOutline,
-    }
-  }
-}
-  // `setup` is a special hook dedicated for the Composition API.
+    };
+  },
+};
+// `setup` is a special hook dedicated for the Composition API.
 </script>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
+import random from 'random'
 //import { LottoExperiment } from './calc.js';
 
-let k_slider = ref(6)
-let n_slider = ref(49)
-let count_trials = ref(10000)
+let k_slider = ref(6);
+let n_slider = ref(49);
+let count_trials = ref(10000);
 
-const trial_counter_value = ref(0)
+const trial_counter_value = ref(0);
 
-const status_text = ref("Status: Die Simulation wurde noch nicht gestartet...")
-const start_stop_text = ref("Simulation starten")
+const status_text = ref("Status: Die Simulation wurde noch nicht gestartet...");
+const start_stop_text = ref("Simulation starten");
 
-const stopIsHidden = ref(true)
-const playIsHidden = ref(false)
+const stopIsHidden = ref(true);
+const playIsHidden = ref(false);
 
-//let start_stop_text = ref("Simulation stoppen")
-//let status_text = ref("Status: Simulation läuft...")
+let simulation_running = false;
+let currently_simulating = false;
 
-defineExpose({
-  k_slider,
-  n_slider,
-  count_trials,
-})
+const name = ref("Vue.js");
 
-let simulation_running = false
-let currently_simulating = false
-
-const name = ref('Vue.js')
-
-function start_stop_animation(event) {
-  //alert(`Hello ${name.value}!`)
-  // `event` is the native DOM event
-  //if (event) {
-  //    alert(event.target.tagName)
-  //}
+async function start_stop_animation(event) {
   if (simulation_running == false) {
-    console.log(simulation_running)
-    simulation_running = true
-    start_stop_text.value = "Simulation stoppen"
-    status_text.value = "Status: Simulation läuft..."
-    playIsHidden.value = true
-    stopIsHidden.value = false
-    const count_wins = LottoExperiment(n_slider,k_slider,count_trials)
-  } else (simulation_running == true) {
-    console.log(simulation_running)
-    simulation_running = false
-    start_stop_text.value = "Simulation starten"
-    status_text.value = "Status: Die Simulation wurde noch nicht gestartet..."
-    playIsHidden.value = false
-    stopIsHidden.value = true
+    console.log(simulation_running);
+    simulation_running = true;
+    start_stop_text.value = "Simulation stoppen";
+    status_text.value = "Status: Simulation läuft...";
+    playIsHidden.value = true;
+    stopIsHidden.value = false;
+    const count_wins = await LottoExperiment(n_slider.value, k_slider.value, count_trials.value);
+    simulation_running = false;
+    start_stop_text.value = "Simulation starten";
+    status_text.value = "Status: Die Simulation wurde noch nicht gestartet...";
+    playIsHidden.value = false;
+    stopIsHidden.value = true;    
+  } else if (simulation_running == true) {
+    console.log(simulation_running);
+    simulation_running = false;
+    start_stop_text.value = "Simulation starten";
+    status_text.value = "Status: Die Simulation wurde noch nicht gestartet...";
+    playIsHidden.value = false;
+    stopIsHidden.value = true;
   } else {
-    alert("Etwas ist schiefgelaufen. Die Webseite wird nun automatisch neu geladen...")
+    alert(
+      "Etwas ist schiefgelaufen. Die Webseite wird nun automatisch neu geladen...",
+    );
     location.reload();
   }
-
-
 }
 
-
-
-
-
 function factorial(n) {
-// n!
-   var fac = 1;
-   for (i = 1; i <= n; i++) fac = fac*i;
-   return fac;
+  // n!
+  var fac = 1;
+  for (let i = 1; i <= n; i++) {
+    fac = fac * i;
+  }
+  return fac;
 }
 
 function binomial(n, k) {
-// Binomialkoeffizient von n über k
-   var bin = factorial(n)/(factorial(k)*factorial(n-k));
-   return bin;
+  // Binomialkoeffizient von n über k
+  var bin = factorial(n) / (factorial(k) * factorial(n - k));
+  return bin;
 }
 
 function Prob(n, k) {
-// Wahrscheinlichkeit für genau r Richtige bei einem Lottofeld
-   var N = binomial(n, k);
-   var y = 1/N
-   return y;
+  // Wahrscheinlichkeit für genau r Richtige bei einem Lottofeld
+  var N = binomial(n, k);
+  var y = 1 / N;
+  return y;
 }
 
 function LottoExperiment(n, k, number_trials) {
-   const prob = Prob(n, k)
-   let count_wins = 0
-   for (let i = 0; i < number_trials; i++) {
-      random_number = random.float((min = 0), (max = 1))
-      if (random_number <= prob) {
-         count_wins++
+  const prob = Prob(n, k)
+  let count_wins = 0
+  trial_counter_value.value = 0
+  for (let i = 0; i < number_trials; i++) {
+    let random_number = Math.random()
+    if (random_number <= prob) {
+      count_wins = count_wins++
+      trial_counter_value.value = i + 1
+      if (i % 1000 == 0) {
+        print("another 1000 done");
       }
-   }
-   return count_wins
+    }
+  }
+  trial_counter_value.value = 0;
+  console.log("Juhu fertig, Gewinn-Zähler: " + String(count_wins));
+  return count_wins;
 }
-
 </script>
-
-
 
 <style scoped>
 @reference "../styles/tailwind.css";
