3553509a3090e66240168d1df6aafd74e48afa4f
Ernest Bernest <ernestbernest@MacBook-Pro-von-User.local>
2026-05-14 18:46:49 +0200

    grid layout works

--- src/components/SimulationConfiguration.vue
+++ src/components/SimulationConfiguration.vue
@@ -1,88 +1,69 @@
 <template>
-  <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-2 gap-y-3 grid-flow-row-dense'>
-    <div className='bg-green-500 rounded-lg shadow-xl min-h-[50px] row-span-2'>
-    </div>
-    <div className='rounded-lg shadow-xl min-h-[50px] row-span-2' id="div_slider">
-
-        <h1
-          class="relative top-0 w-fit py-4 justify-center text-yellow-300 flex items-center bg-clip-text text-7xl font-extrabold text-center select-auto">
-          {{ "LOTTO: " + k_slider + " aus " + n_slider }}
-        </h1>
-        <v-card-text>
-                <div class="text-body-small lg">
+  <div class="grid-container">
+    <div class="box" style="grid-area: box1;">
+    <h1
+      class="relative top-0 w-fit py-4 justify-center text-yellow-300 flex items-center bg-clip-text text-7xl font-extrabold text-center select-auto">
+      {{ "LOTTO: " + k_slider + " aus " + n_slider }}
+    </h1>
+    <v-card-text>
+      <div class="text-body-small lg">
         Show thumb when using slider
       </div>
-          <v-slider v-model="k_slider" :min="1" :max="30" :step="1" class="ma-4" lglabel="k: Anzahl der gezogenen Kugeln"
-            hide-details>
-            <template v-slot:append>
-              <v-text-field v-model="k_slider" density="compact" style="width: 150px" type="number" variant="outlined"
-                hide-details></v-text-field>
-            </template>
-          </v-slider>
-                <div class="text-body-small">
+      <v-slider v-model="k_slider" :min="1" :max="30" :step="1" class="ma-4" lglabel="k: Anzahl der gezogenen Kugeln"
+        hide-details>
+        <template v-slot:append>
+          <v-text-field v-model="k_slider" density="compact" style="width: 150px" type="number" variant="outlined"
+            hide-details></v-text-field>
+        </template>
+      </v-slider>
+      <div class="text-body-small">
         Show thumb when using slider
       </div>
-          <v-slider v-model="n_slider" :min="1" :max="100" :step="1" class="ma-4" lglabel="n: Anzahl der Kugeln insgesamt"
-            hide-details>
-            <template v-slot:append>
-              <v-text-field v-model="n_slider" density="compact" style="width: 150px" type="number" variant="outlined"
-                hide-details></v-text-field>
-            </template>
-          </v-slider>
-                <div class="text-body-small">
+      <v-slider v-model="n_slider" :min="1" :max="100" :step="1" class="ma-4" lglabel="n: Anzahl der Kugeln insgesamt"
+        hide-details>
+        <template v-slot:append>
+          <v-text-field v-model="n_slider" density="compact" style="width: 150px" type="number" variant="outlined"
+            hide-details></v-text-field>
+        </template>
+      </v-slider>
+      <div class="text-body-small">
         Show thumb when using slider
       </div>
-          <v-slider v-model="count_trials" :min="1000" :max="5000000000" :step="1000" class="ma-4"
-            lglabel="Anzahl der gekauften LOTTO-Scheine" hide-details>
-            <template v-slot:append>
-              <v-text-field v-model="count_trials" density="compact" style="width: 150px" type="number"
-                variant="outlined" hide-details></v-text-field>
-            </template>
-          </v-slider>
-        </v-card-text>
+      <v-slider v-model="count_trials" :min="1000" :max="5000000000" :step="1000" class="ma-4"
+        lglabel="Anzahl der gekauften LOTTO-Scheine" hide-details>
+        <template v-slot:append>
+          <v-text-field v-model="count_trials" density="compact" style="width: 150px" type="number" variant="outlined"
+            hide-details></v-text-field>
+        </template>
+      </v-slider>
+    </v-card-text>
+</div>
+<div class="box" style="grid-area: box2;"></div>
+<div class="box" style="grid-area: box5;">
+    <v-data-table v-model:sort-by="sortBy" density="compact" :items="experiments"
+      v-model:items-per-page="itemsPerPage"></v-data-table>
+</div>
+<div class="box" style="grid-area: box3;">
+    <v-btn id="btn-start-sim" block @click="start_simulation" rounded="0" :class="{ hidden: playIsHidden }">
+      <svg-icon type="mdi" :path="pathStart"></svg-icon>
+      {{ start_text }}
+    </v-btn>
+</div>
 
 
-
-    </div>
-    
-    <div className='bg-yellow-500 rounded-lg shadow-xl min-h-[50px] row-span-6 col-span-1'>
-
-      <v-data-table v-model:sort-by="sortBy" density="compact" :items="experiments" v-model:items-per-page="itemsPerPage"></v-data-table>
-    </div>
-    <div className='bg-orange-500 rounded-lg shadow-xl min-h-[50px] col-span-1'>
-
-      <v-btn id="btn-start-sim" block @click="start_simulation" rounded="0" :class="{ hidden: playIsHidden }">
-        <svg-icon type="mdi" :path="pathStart"></svg-icon>
-        {{ start_text }}
-      </v-btn>
-    </div>
+<div class="box" style="grid-area: box4;">
+    <p class="card-simulation-status">{{ status_text }}</p>
+    <p>
+      {{ count_wins_text_field }}
+    </p>
+</div>
 
 
-
-
-    <div className='bg-indigo-500 rounded-lg shadow-xl min-h-[50px]'>
-        <p class="card-simulation-status">{{ status_text }}</p>
-        </div>
-    <div className='bg-purple-500 rounded-lg shadow-xl min-h-[50px] row-span-1'>
-      <p>
-        {{ count_wins_text_field }}
-      </p>
-      </div>
-    <div className='bg-pink-500 rounded-lg shadow-xl min-h-[50px]' />
-    <div className='bg-slate-500 rounded-lg shadow-xl min-h-[50px]' />
+<div class="box" style="grid-area: box6;">
+  <span class="card-simulation-status">{{ k_slider + " Richtige" }}</span>
+  <span class="card-simulation-status">{{ count_wins }}</span>
+</div>
   </div>
-
-  <!---<v-btn rounded="0">
-        <svg-icon type="mdi" :path="pathRestart"></svg-icon>
-        Simulation stoppen und neustarten
-    </v-btn>-->
-
-
-
-  <v-card class="card-simulation-status">{{ k_slider + " Richtige" }}</v-card>
-  <v-card class="card-simulation-status">{{ count_wins }}</v-card>
-
-
 </template>
 
 <script lang="js">
@@ -110,7 +91,7 @@ export default {
 <script setup lang="js">
 import { ref } from "vue"
 
-const worker = new Worker("../worker.js");
+//const worker = new Worker("../worker.js");
 
 
 
@@ -146,11 +127,11 @@ function start_simulation() {
     status_text.value = "Status: Simulation läuft..."
     playIsHidden.value = true
     stopIsHidden.value = false
-    /*count_wins.value = LottoExperiment(
+    count_wins.value = LottoExperiment(
       n_slider.value,
       k_slider.value,
-      count_trials.value)*/
-    worker.postMessage("hello,woker")
+      count_trials.value)
+    //worker.postMessage("hello,woker")
     playWinCounterAnimation(count_wins.value)
     simulation_running = false
     status_text.value = "Status: Die Simulation wurde noch nicht gestartet..."
@@ -212,12 +193,12 @@ function LottoExperiment(n, k, number_trials) {
   console.log("Juhu fertig, Gewinn-Zähler: " + String(count_wins.value))
   count_completed_experiments = count_completed_experiments + 1
   let new_experiment_list_item = {
-      "Experiment Nr.": String(count_completed_experiments),
-      "k": String(k),
-      "n": String(n),
-      "Versuchsanzahl": String(number_trials),
-      "Gewinnanzahl": String(count_wins.value)
-    }
+    "Experiment Nr.": String(count_completed_experiments),
+    "k": String(k),
+    "n": String(n),
+    "Versuchsanzahl": String(number_trials),
+    "Gewinnanzahl": String(count_wins.value)
+  }
   experiments.value.push(new_experiment_list_item)
   return count_wins.value
 }
@@ -258,5 +239,28 @@ div {
 
 #div_slider {
   display: block;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  gap: 1em;
+  grid-template-areas: 
+  "box1 box2"
+  "box1 box2"
+  "box1 box2"
+  "box1 box2"
+  "box1 box2"
+  "box3 box2"
+  "box4 box2"
+  "box5 box6"
+  "box5 box6"
+  "box5 box6"
+  "box5 box6"
+  "box5 box6"
+  "box5 box6"
+  "box5 box6";
+  
 }
 </style>
