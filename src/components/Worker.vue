e92609e871e333672a561e82f5247098022265ce
Ernest Bernest <johannes.schmid@rwth-aachen.de>
2026-05-21 21:57:59 +0200

    state management 4

--- src/components/Worker.vue
+++ src/components/Worker.vue
@@ -22,6 +22,9 @@ import { k_slider } from '../assets/store'
 import { n_slider } from '../assets/store'
 import { count_trials } from '../assets/store'
 import { status_text } from '../assets/store'
+//import { factorial } from "@mathjs"
+
+//const { factorial } = require('mathjs')
 
 let term = ref();
 let fibonnaciValue = ref();
@@ -33,7 +36,7 @@ function fibonacci(num) {
 }
 
 function lotto_experiment(n,k,number_trials) {
-  const prob = Prob(n, k)
+  const prob = 1/(Math.factorial(n) / (Math.factorial(k) * Math.factorial(n - k)))//(binomial(n, k))//Prob(n, k)
   //console.log("prob is " + String(prob))
   let count_wins_local = 0
   /*console.log("n ist " + String(n))
@@ -59,6 +62,53 @@ function lotto_experiment(n,k,number_trials) {
 
 const { workerFn } = useWebWorkerFn(lotto_experiment);
 
+/*function factorial(n) {
+  // n!
+  var fac = 1
+  for (let i = 1; i <= n; i++) {
+    fac = fac * i
+  }
+  return fac
+}*/
+
+function binomial(n, k) {
+  // Binomialkoeffizient von n über k
+  var bin = factorial(n) / (factorial(k) * factorial(n - k))
+  return bin
+}
+
+function Prob(n, k) {
+  // Wahrscheinlichkeit für genau r Richtige bei einem Lottofeld
+  var N = binomial(n, k)
+  var result = 1 / N
+  return result
+}
+
+/*function LottoExperiment(n, k, number_trials) {
+  const prob = 1/(math.factorial(n) / (math.factorial(k) * math.factorial(n - k)))//(binomial(n, k))//Prob(n, k)
+  //console.log("prob is " + String(prob))
+  let count_wins_local = 0*/
+  /*console.log("n ist " + String(n))
+  console.log("k ist " + String(k))
+  console.log("pls make alert before calc")
+  console.log("number_trials ist " + String(number_trials))*/
+  /*for (let i = 0; i < number_trials; i++) {
+    let random_number = Math.random()
+    if (random_number <= prob) {
+      count_wins_local = count_wins_local + 1
+    }
+  }
+  let new_experiment_list_item = {
+    "Nr.": String(count_completed_experiments),
+    "k": String(k),
+    "n": String(n),
+    "Versuche": number_trials.toLocaleString(),
+    "Gewinne": String(count_wins_local)
+  }
+  count_wins.value = count_wins_local
+  return new_experiment_list_item
+}*/
+
 async function onClick() {
   if (simulation_running.value == false) {
     if (k_slider.value > n_slider.value) {
@@ -75,7 +125,7 @@ async function onClick() {
       if (k_slider.value == 6 && n_slider.value == 49) {
         winCostCalcActive.value = true
         cost_sum.value = count_trials.value * price_lottoschein
-        win_sum.value = count_wins.value * 1000000 * (Prob(49, 6)/Prob(n_slider.value, k_slider.value))
+        win_sum.value = count_wins.value * 1000000
       } else {
         winCostCalcActive.value = false
         cost_sum.value = 0
@@ -90,53 +140,6 @@ async function onClick() {
 /*function start_simulation() {
 
 }*/
-
-function factorial(n) {
-  // n!
-  var fac = 1
-  for (let i = 1; i <= n; i++) {
-    fac = fac * i
-  }
-  return fac
-}
-
-function binomial(n, k) {
-  // Binomialkoeffizient von n über k
-  var bin = factorial(n) / (factorial(k) * factorial(n - k))
-  return bin
-}
-
-function Prob(n, k) {
-  // Wahrscheinlichkeit für genau r Richtige bei einem Lottofeld
-  var N = binomial(n, k)
-  var result = 1 / N
-  return result
-}
-
-function LottoExperiment(n, k, number_trials) {
-  const prob = Prob(n, k)
-  //console.log("prob is " + String(prob))
-  let count_wins_local = 0
-  /*console.log("n ist " + String(n))
-  console.log("k ist " + String(k))
-  console.log("pls make alert before calc")
-  console.log("number_trials ist " + String(number_trials))*/
-  for (let i = 0; i < number_trials; i++) {
-    let random_number = Math.random()
-    if (random_number <= prob) {
-      count_wins_local = count_wins_local + 1
-    }
-  }
-  let new_experiment_list_item = {
-    "Nr.": String(count_completed_experiments),
-    "k": String(k),
-    "n": String(n),
-    "Versuche": number_trials.toLocaleString(),
-    "Gewinne": String(count_wins_local)
-  }
-  count_wins.value = count_wins_local
-  return new_experiment_list_item
-}
 </script>
 
 <!--<script lang="js">
